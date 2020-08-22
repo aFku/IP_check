dbb79b202bf9cba217d84c4417abbc8dc45f87ca
aFku <zapasowyrowerek@gmail.com>
2020-08-22 14:47:31 +0200

    Changed ALLOWED_HOSTS

--- IP_check/settings.py
+++ IP_check/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '7*%-)!^noqmf&d*6w!$jr=+*+@vfmk+d_z8m(w96pj(_%39a5o'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
