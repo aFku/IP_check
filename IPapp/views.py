d5175227b90463750c86fbe477e713f0ae879f6e
aFku <44970719+aFku@users.noreply.github.com>
2020-11-20 21:12:38 +0100

    Update views.py

--- IPapp/views.py
+++ IPapp/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect
-from .scripts.getIP import getIP
+import socket
 from .models import Post
 from .forms import PostForm
 
 def ip_view(request):
+    name = socket.gethostname()
+    ip = socket.gethostbyname(name
     if request.method == 'POST':
         form = PostForm(request.POST)
         if form.is_valid():
@@ -11,5 +13,5 @@ def ip_view(request):
             return redirect('/')
     else:
         form = PostForm()
-    return render(request, 'IPapp/ip_site.html', {'ip': getIP(), 'posts': Post.objects.all().order_by('-id'), 'form': form})
+    return render(request, 'IPapp/ip_site.html', {'ip': ip, 'posts': Post.objects.all().order_by('-id'), 'form': form})
 
